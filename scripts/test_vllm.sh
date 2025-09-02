81c1499ba99a1a6c7429f01cf6cb06fb45a89d17
François Paupier <francois.paupier@gmail.com>
2025-09-02 13:07:17 +0200

    reduce(model size)

--- scripts/test_vllm.sh
+++ scripts/test_vllm.sh
@@ -2,7 +2,7 @@ curl http://172.17.0.1:8021/llm-small/v1/completions \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer ecn-llm-token-update-this-secret" \
 -d '{
-    "model": "Qwen/Qwen3-8B",
+    "model": "Qwen/Qwen3-4B",
     "prompt": "Quels sont les sujets enseignés à l école Centrale de Nantes",
     "max_tokens": 128,
     "temperature": 0.8
