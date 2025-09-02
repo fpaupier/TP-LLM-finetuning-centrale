4a101eb99c5f08f62a91bb097f608460b0fd675b
François Paupier <francois.paupier@gmail.com>
2025-09-02 13:06:22 +0200

    add(default API key)

--- scripts/start_vllm.sh
+++ scripts/start_vllm.sh
@@ -3,4 +3,5 @@ docker run --runtime nvidia --gpus all \
     -p 8000:8000 \
     --ipc=host \
     vllm/vllm-openai:latest \
+    --api-key ecn-llm-token-update-this-secret \
     --model Qwen/Qwen3-8B
